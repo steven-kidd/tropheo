78bff83dc2e14b361fc53d235e6680c691942eef
William Graf <William.Graf@kp.org>
2017-04-26 16:21:32 -0700

    finishing touches

--- docker-entrypoint.sh
+++ docker-entrypoint.sh
@@ -12,23 +12,22 @@ cp $APP_DIR/nginx.tropheo.conf /etc/nginx/sites-available/
 ln -s /etc/nginx/sites-available/nginx.tropheo.conf /etc/nginx/sites-enabled/
 echo "daemon off;" >> /etc/nginx/nginx.conf
 
-# ip=`awk '/django/ {print $1; exit}' /etc/hosts`; sed -i "s//$ip/g" /etc/nginx/nginx.conf
-
 # Apply database migrations
 echo Performing database migration.
 python $APP_DIR/manage.py makemigrations
 python $APP_DIR/manage.py migrate
 
-# start outputting logs to stdout
+
+# Start outputting logs to stdout
 tail -n 0 -f $LOG_DIR/*.log &
 # Start gunicorn processes
 echo Starting Gunicorn and nginx. &
 exec gunicorn tropheo.wsgi:application \
-    --name tropheo \
-    --bind unix:$APP_DIR/tropheo.sock \
-    --workers 3 \
+    --name=tropheo \
+    --bind=unix:$APP_DIR/tropheo.sock \
+    --workers=3 \
     --log-level=info \
-    --timeout 300 \
+    --timeout=300 \
     --log-file=$LOG_DIR/gunicorn.log \
     --access-logfile=$LOG_DIR/access.log & 
 exec service nginx start
